6379f3e9e36e12499b8c922742be64ec18774348
Tomasz Podsiadlik <tpodsiadlik@future-processing.com>
2020-07-06 12:38:50 +0200

    Fetch products from product service

--- src/app/products/components/product-details/product-details.component.ts
+++ src/app/products/components/product-details/product-details.component.ts
@@ -1,10 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/Product';
+import { ProductService } from 'src/app/core/services/product-service.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product: Product;
 
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('product_id');
+    this.productService.getProductById(id).subscribe({
+      next: product => {
+        this.product = product
+      },
+      error: error => console.log(error)
+    })
+  }
 }
--- src/app/products/components/products-list/products-list.component.ts
+++ src/app/products/components/products-list/products-list.component.ts
@@ -1,47 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../../../models/Product';
+import { ProductService } from 'src/app/core/services/product-service.service';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.scss']
 })
-export class ProductsListComponent {
-  products: Product[] = [
-    {
-      id: 1,
-      name: "Mleko",
-      description: 'lorem loremloremloremloremloremloremloremlorem loremloremv loremlorem',
-      price: 23
-    },
-    {
-      id: 2,
-      name: "Chleb",
-      description: 'lorem loremloremloremloremloremloremloremlorem loremloremv loremlorem',
-      price: 1
-    },
-    {
-      id: 3,
-      name: "Masło",
-      description: 'lorem loremloremloremloremloremloremloremlorem loremloremv loremlorem',
-      price: 99
-    },
-    {
-      id: 4,
-      name: "Kalafior",
-      description: 'lorem loremloremloremloremloremloremloremlorem loremloremv loremlorem',
-      price: 0
-    },
-    {
-      id: 5,
-      name: "Ser",
-      description: 'lorem loremloremloremloremloremloremloremlorem loremloremv loremlorem',
-      price: 0
-    },
-  ]
+export class ProductsListComponent implements OnInit {
+  products: Product[];
 
-  handleDetailsClick(id) {
-    console.log(id)
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getAllProducts().subscribe({
+      next: products => {
+        this.products = products
+      },
+      error: error => console.log(error)
+    })
   }
 }
 
